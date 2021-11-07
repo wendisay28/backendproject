41e236fbd64dde4a26379c97885242f5c936d8ec
wendisay28 <wendy.nietov@gmail.com>
2021-11-06 19:10:14 -0500

    auth middleware

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,12 +3,10 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const msg = require('../helpers/messages')
 
-
-
 const authService = {
-    signToken: async(id)=>{
-        return jwt.sign({id}, process.env.JWT_SECRET, {
-            expiresIn: 68 * 60 *24 * 365
+    signToken: async function(_id){
+        return jwt.sign({ _id }, process.env.JWT_SECRET, {
+            expiresIn: 60 * 60 * 24 * 365
         })
     },
     login: async function(data){
@@ -24,24 +22,24 @@ const authService = {
             }else{
                 return msg.authFailed
             }
-        }catch (error){
+        } catch (error) {
             return error
         }
     },
-    register: async ()=>{
-        try{
+    register: async function(userData){
+        try {
             let hash = await bcrypt.hash(userData.password, 10).then(res => res)
             userData.password = hash
             await userData.save()
-            let token = await this.signToken(userData.id)
-            return{
+            let token = await this.signToken(userData._id)
+            return {
+                userData,
                 code: 200,
                 token
             }
-        }catch(error){
+        } catch (error) {
             return error
         }
-
     }
 }
 
