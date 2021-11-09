647a0adfeb1eef67f97102503d8bfb11ade15808
wendisay28 <wendy.nietov@gmail.com>
2021-11-09 18:54:39 -0500

    validator

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -133,7 +133,7 @@ router.post('/register', [
     async (req, res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(422).json({ errors: errors.array() });
     }
     try{
         const user = new User(req.body)
